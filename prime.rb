1d00d4adfa7456206a78a823184838ef5343fb9e
sensible-shell-8529 <diana.lee116@gmail.com>
2019-02-03 14:25:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 def prime?(num)
   if num ==1 
-    return true 
-    elsif num<1 
-    return false
- else for x in 2..num-1
+  return true 
+  elsif num<1 
+  return false
+  else for x in 2..num-1
   if (num%x) ==0 
     return false 
   end
